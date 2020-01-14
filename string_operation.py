5bbdd70c3c4a9718a9d988883aca59d1cd33b5ab
Robbot-Zhao <robbot19820626@163.com>
2020-01-14 09:48:56 +0800

    add new string-operation file

---
+++ string_operation.py
@@ -0,0 +1,10 @@
+# Python String Operation
+
+str_name = "bAicells IS gooDE"
+print(str_name.title()) # change the first character to Upper, others keep lower
+
+print(str_name.upper()) # change all characters to Upper
+print(str_name.lower()) # change all characters to lower
+
+
+
